6079fcee2e3936ba2f9de4a5f686edb6be459352
Andrew Hill <andrew@andrewkhill.com>
2020-05-23 16:16:32 -0500

    My version

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* user and group to drop privileges to */
 static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
+	[INIT] =   "#222222",     /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
-	[CAPS] = "red",         /* CapsLock on */
+	[CAPS] = "yellow",         /* CapsLock on */
 };
 
 /* treat a cleared input like a wrong password (color) */
@@ -19,4 +19,4 @@ static const char * message = "Suckless: Software that sucks less.";
 static const char * text_color = "#ffffff";
 
 /* text size (must be a valid size) */
-static const char * font_name = "6x10";
+static const char * font_name = "lucidasanstypewriter-14";
